36a15b2ef13029d3a3c81502c3d6aef6a44150f5
mohamed7431 <mohamedni7431@gmail.com>
2026-04-03 15:48:32 +0530

    Update views.py

--- core/views.py
+++ core/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from django.http import JsonResponse
-from django.contrib.auth.decorators import login_required
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
